977d268c76e6e502c4185c4a008c502d08bdc0c4
seasayDev <saliouseasay@gmail.com>
2022-05-18 01:23:19 -0400

    Add the weather script file

---
+++ script.js
@@ -0,0 +1,45 @@
+let weather = {
+    apikey: "5bc2b4f40acfffa046713955a4370d52",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apikey
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { description, icon } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      console.log(name, icon, description, temp, humidity, speed);
+      document.querySelector(".location-city").innerHTML = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      document.querySelector(".temperature-degree").innerHTML = temp + "°C";
+      document.querySelector(".humidity").innerHTML =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerHTML = "Wind: " + speed + "Km/hr";
+      document.querySelector(".weather ").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + description + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") weather.search();
+    });
+  weather.fetchWeather("Montreal");
